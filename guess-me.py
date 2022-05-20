2fd73460f779bbaad74eb3c1c469b8c444758025
Vikash Batham <vikashbatham97@gmail.com>
2022-05-20 20:16:00 +0530

    fixed readme and code

--- guess-me.py
+++ guess-me.py
@@ -1,13 +1,11 @@
 from random import randint
 
+
 # generated random number to be guessed by player
 random_num = randint(1, 101)
 
-print(random_num)
-
 print('#'*20)
 print('#'*20)
-print('\n')
 print("WELCOME TO GUESS ME!")
 print("I'm thinking of a number between 1 and 100")
 print("If your guess is more than 10 away from my number, I'll tell you you're COLD")
@@ -15,7 +13,6 @@ print("If your guess is within 10 of my number, I'll tell you you're WARM")
 print("If your guess is farther than your most recent guess, I'll say you're getting COLDER")
 print("If your guess is closer than your most recent guess, I'll say you're getting WARMER")
 print("LET'S PLAY!")
-print('\n')
 print('#'*20)
 print('#'*20)
 print('\n')
